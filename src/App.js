f05db2a791f000bd0e73deab58a6729b6bee9790
unknown <hitprofit70@gmail.com>
2023-01-06 09:40:59 +0200

    HashLoader spinner added

--- src/App.js
+++ src/App.js
@@ -6,20 +6,39 @@ import Women from './pages/Women';
 import Cart from './pages/Cart';
 import MyAccount from './pages/MyAccount';
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import HashLoader from "react-spinners/HashLoader";
 
 
 function App() {
+
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000)
+
+  }, [])
+
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Logo />} />
-        <Route path="sale" element={<Sale />} />
-        <Route path="/men" element={<Men />} />
-        <Route path="/women" element={<Women />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path="/myaccount" element={<MyAccount />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+
+      {
+
+        loading ?
+          <HashLoader color={'#1f0ac4'} loading={loading} size={100} className='spinner-HashLoader'/>
+          :
+          <Routes>
+            <Route path="/" element={<Logo />} />
+            <Route path="sale" element={<Sale />} />
+            <Route path="/men" element={<Men />} />
+            <Route path="/women" element={<Women />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path="/myaccount" element={<MyAccount />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+      }
     </div>
   );
 }
